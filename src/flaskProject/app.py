cac7f1db0e9b7658329bb91c11282eebae9fce26
Nicolas Frache <nicolas.frache@telecomnancy.eu>
2021-12-05 15:40:07 +0100

    ✨ NEW: template header + deconnexion
    - Ajout du header qui est sur l'ensemble des page
    - Gestion de la connexion avec un cookie
    - Affichage de l'état de connexion dans le header
    - Bouton pour se déconnecter dans le header

--- src/flaskProject/app.py
+++ src/flaskProject/app.py
@@ -18,21 +18,53 @@ def connect_database(getdict=False):
     return db
 
 
+def is_logged():
+    return not request.cookies.get("username") is None
+
+
+def get_header():
+    header = {"is_logged": is_logged()}
+    if header["is_logged"]:
+        header["username"] = request.cookies.get("username")
+    # TODO: prendre en compte le rôle pour un header différent
+    return header
+
+
 @app.route('/')
 def home():
-    return render_template("pages/home.html")
+    return render_template("pages/home.html", header=get_header())
 
 
 @app.route('/listeCitoyens')
-def sandbox():
+def lister_citoyens():
     try:
         with connect_database(getdict=True).cursor() as cursor:
             cursor.execute("SELECT id as 'Numéro', Prenom, Nom, Profession "
                            "from utilisateur")
             data = cursor.fetchall()
     except Exception as e:
-        return render_template("pages/error.html", error=str(e))
-    return render_template("pages/listeCitoyens.html", data_tab=data)
+        return render_template("pages/error.html", error=str(e), header=get_header())
+    return render_template("pages/listeCitoyens.html", data_tab=data, header=get_header())
+
+
+@app.route('/sandbox')
+def sandbox():
+    compteur = request.cookies.get("compteur")
+    if compteur is None:
+        val = "0"
+    else:
+        val = int(compteur) + 1
+    resp = make_response(render_template("pages/sandbox.html", compteur=compteur, header=get_header()))
+    resp.set_cookie("compteur", str(val))
+    return resp
+
+
+@app.route("/seDeconnecter")
+def se_deconnecter():
+    newUrl = request.args.get("redirect", default="/")
+    resp = make_response(redirect(newUrl))
+    resp.set_cookie('username', '', expires=0)
+    return resp
 
 
 if __name__ == '__main__':
